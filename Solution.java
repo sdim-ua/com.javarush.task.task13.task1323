ee280277bf7be26b5150f3fd1f23eb9186641882
sdim-ua <ingersol@yandex.ua>
2019-06-26 22:40:08 +0400

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,27 @@
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+
+    interface Selectable {
+        void onSelect();
+    }
+
+    interface Updatable extends Selectable {
+        void refresh();
+    }
+
+    class Screen implements Updatable {
+        @Override
+        public void onSelect(){
+            
+        }
+        
+        @Override
+        public void refresh(){
+            
+        }
+    
+
+    }
+}
